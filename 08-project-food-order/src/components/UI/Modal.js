84f84294e60a7c3a4ed8e59624e9143ef26f7b22
rakahikmah <rakahikmah46@gmail.com>
2023-04-11 17:15:52 +0700

    Course 146 Playist

--- 08-project-food-order/src/components/UI/Modal.js
+++ 08-project-food-order/src/components/UI/Modal.js
@@ -19,9 +19,9 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
       {ReactDOM.createPortal(
-        <ModalOverlay>{props.children}/</ModalOverlay>,
+        <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
       )}
     </Fragment>
