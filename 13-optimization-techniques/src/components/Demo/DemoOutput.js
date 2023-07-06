32ee2c7dea1518311f0362142364b39ec4e6619b
rakahikmahcz <rakharamadhan@cashlez.com>
2023-07-06 07:29:53 +0700

    use callback preventing

--- 13-optimization-techniques/src/components/Demo/DemoOutput.js
+++ 13-optimization-techniques/src/components/Demo/DemoOutput.js
@@ -2,7 +2,6 @@ import React from "react";
 import MyParagraph from "./MyParagraph";
 
 const DemoOutput = (props) => {
-  console.log("DemoOutput RUNNING");
   return <MyParagraph>{props.show ? "This is now" : ""}</MyParagraph>;
 };
 
--- 13-optimization-techniques/src/components/Demo/MyParagraph.js
+++ 13-optimization-techniques/src/components/Demo/MyParagraph.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const MyParagraph = (props) => {
-  console.log("MyParagraph RUNNING");
   return <p>{props.children}</p>;
 };
 
